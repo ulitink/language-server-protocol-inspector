aaf5477745e6ae70c5f4b6025820015d9898b771
Konstantin Ulitin <ulitin.k@gmail.com>
2024-05-07 19:48:46 +0200

    publish to dist

--- lsp-inspector/vue.config.js
+++ lsp-inspector/vue.config.js
@@ -1,11 +1,11 @@
 const BASEURL = ''
 // const BASEURL = process.env.TARGET === 'lsp-website' ? '/language-server-protocol/inspector' : '/'
 
-const OUTPUT_DIR =
-  process.env.TARGET === 'lsp-website' ? 'dist' : '../lsp-inspector-webview/dist/src'
+const OUTPUT_DIR = '../docs';
 
 module.exports = {
   baseUrl: BASEURL,
+  outputDir: OUTPUT_DIR,
   devServer: {
     overlay: false
   },
